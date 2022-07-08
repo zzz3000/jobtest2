8a353da3e6a4beb5e8a8ba1e1267abffe0395073
zzz3000 <sanek_21_495@mail.ru>
2022-07-08 11:07:32 +0300

    security

--- jt/src/main/java/org/zzz/jt/security/SecurityConfig.java
+++ jt/src/main/java/org/zzz/jt/security/SecurityConfig.java
@@ -1,11 +1,8 @@
 package org.zzz.jt.security;
 
 import javax.servlet.http.HttpServletResponse;
-
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,62 +10,35 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.zzz.jt.data.User;
 import org.zzz.jt.data.UserWithDetails;
 import org.zzz.jt.repository.UserRepository;
-import org.zzz.jt.service.UserService;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	
-	//@Autowired
-    //private UserService service;
-     
 	@Autowired
 	UserRepository userRepo;
-	
+
 	@Autowired
 	JwtTokenFilter jwtTokenFilter;
 
-	/*
 	@Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-				
-		auth.userDetailsService(username ->  userRepo
-			      .getByName(username)
-			      .orElseThrow(
-			        () -> new UsernameNotFoundException(
-			          String.format("User: %s, not found", username))));
-		
-		//auth.
-	}
-	*/
-	
-	@Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-				
-		auth.userDetailsService(username ->  
-			{User u = userRepo.getById(Integer.parseInt(username));
-				if(u==null) {
-					throw new UsernameNotFoundException(
-					          String.format("User: %s, not found", username));
-				}
-			    
-			    return new UserWithDetails(u);
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		auth.userDetailsService(username -> {
+			User u = userRepo.getById(Integer.parseInt(username));
+			if (u == null) {
+				throw new UsernameNotFoundException(String.format("User: %s, not found", username));
 			}
-			);
-		
-		//auth.
+			return new UserWithDetails(u);
+		});
+
 	}
-	
-	
+
 	@Override
 	@Bean
 	public AuthenticationManager authenticationManagerBean() throws Exception {
@@ -87,56 +57,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http = http.exceptionHandling().authenticationEntryPoint((request, response, ex) -> {
 			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, ex.getMessage());
 		}).and();
-		
-		//TODO
+
 		// Set permissions on endpoints
-        http.authorizeRequests()
-        	.antMatchers("/users/signin").permitAll()//
-        	.antMatchers("/users/signin1").permitAll()//
-        	.antMatchers("/login").permitAll()//
-        	.antMatchers("/hello/user").permitAll()//
-        	
-            // Our public endpoints
-            .antMatchers("/api/public/**").permitAll()
-            .antMatchers(HttpMethod.GET, "/api/author/**").permitAll()
-            .antMatchers(HttpMethod.POST, "/api/author/search").permitAll()
-            .antMatchers(HttpMethod.GET, "/api/book/**").permitAll()
-            .antMatchers(HttpMethod.POST, "/api/book/search").permitAll()
-            // Our private endpoints
-            .anyRequest().authenticated()
-        
-        
-            .and().httpBasic();
-        
-        
-        
-     // Add JWT token filter
-        http.addFilterBefore(
-            jwtTokenFilter,
-            UsernamePasswordAuthenticationFilter.class
-        );
+		http.authorizeRequests().antMatchers("/users/signin").permitAll()//
+				.antMatchers("/users/signin1").permitAll()//
+				.anyRequest().authenticated().and().httpBasic();
+
+		// Add JWT token filter
+		http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
 	}
-	
-	/*
-	// Used by spring security if CORS is enabled.
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source =      new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        CorsFilter  corsFilter = new CorsFilter();
-      
-        return corsFilter;
-    }
-    */
-    
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-       // return new BCryptPasswordEncoder();
-    	return  NoOpPasswordEncoder.getInstance();
-    }
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		// return new BCryptPasswordEncoder();
+		return NoOpPasswordEncoder.getInstance();
+	}
 }
